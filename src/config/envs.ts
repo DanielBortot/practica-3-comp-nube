cbecde78af3e2b966ee40b7b2a76ede3f40f5238
Daniel Bortot <daniel.bortot@gmail.com>
2024-10-23 16:09:34 -0400

    fix: :bug: agregando tag a mongo, cambiando .env y eliminando mongo-express

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,11 +4,15 @@ import * as joi from 'joi';
 interface EnvVars {
     PORT: number;
     DB_HOST: string;
+    MONGO_INITDB_ROOT_USERNAME: string;
+    MONGO_INITDB_ROOT_PASSWORD: string;
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
     DB_HOST: joi.string().required(),
+    MONGO_INITDB_ROOT_USERNAME: joi.string().required(),
+    MONGO_INITDB_ROOT_PASSWORD: joi.string().required()
 }).unknown(true);
 
 const {error, value} = envsSchema.validate(process.env);
@@ -18,5 +22,7 @@ const envVars: EnvVars = value
 
 export const envs = {
     port: envVars.PORT,
-    db_host: envVars.DB_HOST
+    db_host: envVars.DB_HOST,
+    mongo_user: envVars.MONGO_INITDB_ROOT_USERNAME,
+    mongo_pass: envVars.MONGO_INITDB_ROOT_PASSWORD
 }
